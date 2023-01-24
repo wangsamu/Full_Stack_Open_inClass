7c2ce928574424d30d8f3ad09aa992f624b1fb2f
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-01-24 18:38:51 +0100

    Counter added

--- part1/src/Time.tsx
+++ part1/src/Time.tsx
@@ -2,10 +2,24 @@ import React, { useState } from 'react';
 
 function Time() {
   const [time, setTime] = useState(0);
+  const [avocados, setAvocados] = useState(0);
 
-  setInterval(() => setTime(time + 1), 1000);
+  setTimeout(() => setTime(time + 1), 1000);
 
-  return <div>{time}</div>;
+  const handleClick = () => {
+    setAvocados(avocados + 1);
+  };
+
+  return (
+    <div>
+      <p>Time since you opened the app: {time}</p>
+      <p>
+        🥑
+        {Array.from({ length: avocados }, () => '🥑').map((avocado) => avocado)}
+      </p>
+      <button onClick={handleClick}>add an avocado</button>
+    </div>
+  );
 }
 
 export default Time;
