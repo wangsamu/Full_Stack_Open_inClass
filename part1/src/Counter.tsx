78358743a6be78070d7220c163ddc56b92bfd8ed
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-01-25 23:37:57 +0100

    Counter state change to object, holding values of avocados and coconuts

--- part1/src/Counter.tsx
+++ part1/src/Counter.tsx
@@ -3,18 +3,24 @@ import Button from './Button';
 
 function Counter() {
   const [time, setTime] = useState(0);
-  const [avocados, setAvocados] = useState(0);
+  const [fruits, setFruits] = useState({
+    avocados: 0,
+    coconuts: 0,
+  });
 
   setTimeout(() => setTime(time + 1), 1000);
 
   const increaseAvocado = () => {
-    setAvocados(avocados + 1);
+    setFruits({ ...fruits, avocados: fruits.avocados + 1 });
   };
-  const decreaseAvocado = () => {
-    setAvocados(avocados - 1);
+  const increaseCoconut = () => {
+    setFruits({ ...fruits, coconuts: fruits.coconuts + 1 });
   };
-  const resetAvocados = () => {
-    setAvocados(0);
+  // const decreasefruit = () => {
+  //   setAvocados(avocados - 1);
+  // };
+  const resetFruit = () => {
+    setFruits({ avocados: 0, coconuts: 0 });
   };
 
   return (
@@ -22,11 +28,20 @@ function Counter() {
       <p>Time since you opened the app: {time}</p>
       <p>
         Avocados:
-        {Array.from({ length: avocados }, () => '🥑').map((avocado) => avocado)}
+        {Array.from({ length: fruits.avocados }, () => '🥑').map(
+          (avocado) => avocado
+        )}
+      </p>
+      <p>
+        Coconuts:
+        {Array.from({ length: fruits.coconuts }, () => '🥥').map(
+          (avocado) => avocado
+        )}
       </p>
       <Button text={'Add an avocado'} handleClick={increaseAvocado} />
-      <Button text={'Remove an avocado'} handleClick={decreaseAvocado} />
-      <Button text={'Delete all avocados'} handleClick={resetAvocados} />
+      <Button text={'Add a coconut'} handleClick={increaseCoconut} />
+      {/* <Button text={'Remove an avocado'} handleClick={decreasefruit} /> */}
+      <Button text={'Delete all avocados'} handleClick={resetFruit} />
     </div>
   );
 }
