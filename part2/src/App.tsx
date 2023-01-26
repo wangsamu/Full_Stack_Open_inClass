c42164e6ab9a05ae7a70e16e7fa9f7d9fa404a27
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-01-26 23:26:10 +0100

    List rendering refactored

--- part2/src/App.tsx
+++ part2/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { FunctionComponent, useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+interface AppProps {
+  notes: { id: number; content: string; important: boolean }[];
 }
 
-export default App
+const App: FunctionComponent<AppProps> = ({ notes }) => {
+  return (
+    <div className='App'>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.content}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
