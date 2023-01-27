756daad038cec081e13adefaf43b2b025cb36860
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-01-27 10:16:40 +0100

    Add notes functionality implemented

--- part2/src/App.tsx
+++ part2/src/App.tsx
@@ -1,19 +1,48 @@
-import { FunctionComponent, useState } from 'react';
+import React, {
+  FormEvent,
+  FunctionComponent,
+  KeyboardEvent,
+  MouseEvent,
+  SyntheticEvent,
+  useState,
+} from 'react';
 import './App.css';
 
 interface AppProps {
   notes: { id: number; content: string; important: boolean }[];
 }
 
-const App: FunctionComponent<AppProps> = ({ notes }) => {
+const App: FunctionComponent<AppProps> = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [inputValue, setInputValue] = useState('');
+
+  const addNote = (event: SyntheticEvent) => {
+    event.preventDefault();
+    const newId = Math.max(...notes.map((note) => note.id)) + 1;
+    const newNote = {
+      id: newId,
+      content: inputValue,
+      important: false,
+    };
+    setNotes([...notes].concat(newNote));
+  };
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className='App'>
       <h1>Notes</h1>
       <ul>
         {notes.map((note) => (
-          <li key={note.content}>{note.content}</li>
+          <li key={note.id}>{note.content}</li>
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input type='text' value={inputValue} onChange={handleInput} />
+        <button type='submit'>save</button>
+      </form>
     </div>
   );
 };
